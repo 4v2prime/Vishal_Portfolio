920219725a3c5a9622f459701e7c00e7c16b301f
Prime-Property <vishalpardeshi8830@gmail.com>
2025-01-15 18:12:51 +0530

    portfolio builed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ResumeComponent } from "./resume/resume.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, ContactComponent, FooterComponent, PortfolioComponent, ResumeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
